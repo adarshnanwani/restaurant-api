c0591c80689b3ab916e253d793a715e68240becd
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-03-20 22:39:48 +0530

    Add middleware and connect the DB.

--- server.js
+++ server.js
@@ -1,8 +1,34 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
 
-const port = process.env.PORT || 5000;
+// Load env vars
+dotenv.config({
+  path: './config/config.env'
+});
+
+// Connect to DB
+connectDB();
+
+// Load route files
+
+// Initialize app
 const app = express();
 
+/* Apply middleware */
+
+// Enable body-parser(for POST requests)
+app.use(express.json());
+
+// Enable cors
+app.use(cors());
+
+/* Apply middleware ENDS*/
+
+// Mount routers
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`App listening on port ${port}!`);
 });
