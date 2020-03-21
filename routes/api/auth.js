295eb0cf8466a0f1c428b852b5368c0c2b320c27
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-03-21 19:29:50 +0530

    Get currently logged in user data.

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser } = require('../../controllers/auth');
+const { protect } = require('../../middleware/auth');
+const { registerUser, loginUser, getMe } = require('../../controllers/auth');
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
+router.route('/me').get(protect, getMe);
 
 module.exports = router;
