4984fa824c8efa0f04d45bdb7e30e117b12cef6b
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-03-21 19:22:33 +0530

    Add protect middleware to authenticate private routes.

---
+++ middleware/auth.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const asyncHandler = require('./async');
+const ErrorResponse = require('../utils/errorResponse');
+
+// Protect routes -- authenticate
+exports.protect = asyncHandler(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    // set token from auth header
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  // if token doesn't exist
+  if (!token) {
+    return next(new ErrorResponse('Unauthorized access', 401));
+  }
+
+  // Verify token, set user in current request and continue execution
+  try {
+    const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = await User.findById(decoded.id);
+
+    next();
+  } catch (err) {
+    return next(new ErrorResponse('Unauthorized access', 401));
+  }
+});
