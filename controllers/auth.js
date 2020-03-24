996432ecb170396b0e78c020b8993a88e554674a
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-03-24 18:50:04 +0530

    Add forgot password functionality.

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const User = require('../models/User');
 const crypto = require('crypto');
 const asyncHandler = require('../middleware/async');
 const ErrorResponse = require('../utils/errorResponse');
+const sendEmail = require('../utils/sendEmail');
 
 //@desc     Register User
 //@route    POST api/v1/auth/register
@@ -54,6 +55,46 @@ exports.getMe = asyncHandler(async (req, res, next) => {
   const user = await User.findById(req.user.id);
 
   res.status(200).json({ success: true, data: user });
+});
+
+//@desc     Forgot password
+//@route    POST api/v1/auth/forgotpassword
+//@access   Public
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(new ErrorResponse('There is no user with that email', 404));
+  }
+
+  // Get reset token
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  // Create reset url
+  const resetUrl = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/auth/resetpassword/${resetToken}`;
+
+  const message = `You are receiving this email because you (or someone else) has requested the reset of a password.
+   Please make a PUT request to \n\n ${resetUrl}`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Password reset token',
+      message
+    });
+    res.status(200).json({ success: true, data: 'Email sent' });
+  } catch (err) {
+    console.log(err);
+    user.getResetPasswordToken = undefined;
+    user.getResetPasswordExpire = undefined;
+
+    await user.save({ validateBeforeSave: false });
+    return next(new ErrorResponse('Email could not be sent', 500));
+  }
 });
 
 // Get token from model, create cookie and send response
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../../middleware/auth');
-const { registerUser, loginUser, getMe } = require('../../controllers/auth');
+const {
+  registerUser,
+  loginUser,
+  getMe,
+  forgotPassword
+} = require('../../controllers/auth');
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
 router.route('/me').get(protect, getMe);
+router.route('/forgotpassword').post(forgotPassword);
 
 module.exports = router;
