c364a9144b8c40ed4d6a62649d89cd0bd11096d0
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-03-26 18:08:08 +0530

    Add delete menu item functionality.

--- controllers/menuItems.js
+++ controllers/menuItems.js
@@ -19,3 +19,45 @@ exports.addMenuItem = asyncHandler(async (req, res, next) => {
 
   res.status(201).json({ success: true, data: menuItem });
 });
+
+//@desc     Delete a menuitem
+//@route    DELETE /api/v1/menuitems/:menuItemId
+//@access   Private (only for restaurant users)
+exports.deleteMenuItem = asyncHandler(async (req, res, next) => {
+  // Check if the user is a restaurant
+  if (!req.user.isRestaurant) {
+    return next(
+      new ErrorResponse('User not authorized to delete a menu item', 401)
+    );
+  }
+
+  // Check if the menu item exists
+  const menuItem = await MenuItem.findById(req.params.menuItemId);
+
+  if (!menuItem) {
+    return next(
+      new ErrorResponse(
+        `No menu item found with id ${req.params.menuItemId}`,
+        404
+      )
+    );
+  }
+
+  // Check if the menu item belongs to this user
+  if (menuItem.user.toString() !== req.user.id) {
+    return next(
+      new ErrorResponse(
+        `No menu item found with id ${req.params.menuItemId}`,
+        404
+      )
+    );
+  }
+
+  // If everything goes well, then remove the menu item
+  await menuItem.remove();
+
+  res.status(200).json({
+    success: true,
+    data: {}
+  });
+});
--- routes/api/menuItems.js
+++ routes/api/menuItems.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../../middleware/auth');
-const { addMenuItem } = require('../../controllers/menuItems');
+const { addMenuItem, deleteMenuItem } = require('../../controllers/menuItems');
 
 router.route('/').post(protect, addMenuItem);
+router.route('/:menuItemId').delete(protect, deleteMenuItem);
 
 module.exports = router;
