14785191b2ee0b8c3d10a20413b965676010fe80
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-03-29 20:44:22 +0530

    Handle custom errors for image upload.

--- controllers/images.js
+++ controllers/images.js
@@ -10,7 +10,14 @@ exports.uploadImage = asyncHandler(async (req, res, next) => {
     const imageData = await saveImage(req, 'image', folderPath);
     res.status(201).json({ success: true, data: imageData });
   } catch (err) {
-    console.log(err);
+    if (err === 'No image found') {
+      return next(new ErrorResponse('Please send an image file', 400));
+    }
+    if (err === 'Incorrect mimetype') {
+      return next(
+        new ErrorResponse('Please only add a .jpg or .png image file', 400)
+      );
+    }
     res.status(500).json({ success: false, data: err });
   }
 });
