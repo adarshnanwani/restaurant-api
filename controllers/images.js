025a24e23c71a8a5a4e3bb897b80d2b274cb2e45
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-04-06 10:20:56 +0530

    Add new API routes for images.

--- controllers/images.js
+++ controllers/images.js
@@ -4,8 +4,29 @@ const saveImage = require('../utils/saveImage');
 
 //@desc     Upload an image
 //@route    POST /api/v1/images
-//@access   Private
+//@access   Public
 exports.uploadImage = asyncHandler(async (req, res, next) => {
+  const folderPath = `${process.env.CLOUDINARY_ROOT_APP_FOLDER}/presignup`;
+  try {
+    const imageData = await saveImage(req, 'image', folderPath);
+    res.status(201).json({ success: true, data: imageData });
+  } catch (err) {
+    if (err === 'No image found') {
+      return next(new ErrorResponse('Please send an image file', 400));
+    }
+    if (err === 'Incorrect mimetype') {
+      return next(
+        new ErrorResponse('Please only add a .jpg or .png image file', 400)
+      );
+    }
+    res.status(500).json({ success: false, data: err });
+  }
+});
+
+//@desc     Upload an image
+//@route    POST /api/v1/images/user
+//@access   Private
+exports.uploadImageProtected = asyncHandler(async (req, res, next) => {
   const folderPath = `${process.env.CLOUDINARY_ROOT_APP_FOLDER}${
     req.user.isRestaurant ? '/restaurants' : '/users'
   }/${req.user.userEmail}`;
